7cd742ef437f8d339d9133c0c124ea407943eaba
Sajfli <sajmon88@protonmail.ch>
2021-09-28 00:46:46 +0200

    Add endpoints for client

--- src/index.js
+++ src/index.js
@@ -1,13 +1,30 @@
 import express from 'express'
+import path from 'path'
+import { createServer } from 'http'
+import { Server } from 'socket.io'
 
 import router from './Routes/router'
 
 const app = express()
 
-const PORT = 5000
+app.use(express.static(path.join(__dirname, '../client/build')))
 
-// app.use('/static', express.static('static'))
-
+// add routing
 router(app)
 
-app.listen(PORT, () => console.log(`API listening on ${PORT}`))
+// serve client
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname+'/client/build/index.html'))
+})
+
+
+const PORT = process.env.PORT || 5000
+
+const httpServer = createServer(app)
+const io = new Server(httpServer)
+
+io.on('connection', socket => {
+    console.log('connection')
+})
+
+httpServer.listen(PORT)
