f12f9ee8e36377a549d2bfd439a1513303241c93
Sajfli <sajmon88@protonmail.ch>
2021-11-12 22:24:24 +0100

    add socket endpoints for rooms

--- src/socket.js
+++ src/socket.js
@@ -14,20 +14,31 @@ export default httpServer => {
         console.log('connection')
 
         // idk
-        const roomId = '2137'
+        // const roomId = '2137'
 
         // send random id (todo)
         socket.on('get room id', () => {
 
-            socket.emit('room id', roomId)
+            const randomId = Math.random().toString(36).substring(2, 13)
+
+            // socket.room = randomId
+
+            socket.emit('room id', randomId)
         })
 
         // room join, room id is needed
         socket.on('room join', roomId => {
             console.log('a client wants to join room ' + roomId)
-            socket.join(roomId)
 
-            socket.emit('room joined', roomId)
+
+            socket.join(roomId)
+            socket.room = roomId
+
+            // const video = io.sockets.adapter.rooms.get(socket.room).video
+            // if(video)
+            //     socket.emit('room joined', {roomId, video})
+            // else
+                socket.emit('room joined', roomId)
 
             socket.to(roomId).emit('userConnected')
         })
@@ -35,20 +46,33 @@ export default httpServer => {
         // change video
         socket.on('new video', data => {
             // socket.broadcast.to()
+            if(!socket.room) return
             console.log('user sent new video!')
             // const room = socket.rooms[1]
 
-            socket.to(roomId).emit('new video', data)
+            // console.log(socket.room , io.sockets.adapter.rooms.get(socket.room))
+            // io.sockets.adapter.rooms.get(socket.room).video = data
+
+            socket.to(socket.room).emit('new video', data)
         })
 
         // seek
         socket.on('seek', t => {
-            socket.to(roomId).emit('seek', t)
+            socket.to(socket.room).emit('seek', t)
         })
 
         // pause / unpause
         socket.on('playing', state => {
-            socket.to(roomId).emit('playing', state)
+            socket.to(socket.room).emit('playing', state)
+        })
+
+        // get watchtime after new user enter
+        socket.on('get watchtime', () => {
+            socket.to(socket.room).emit('get watchtime')
+        })
+
+        socket.on('send watchtime', ({video, data}) => {
+            console.log(video, data)
         })
 
 
